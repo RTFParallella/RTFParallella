a871acd4ef3a8c8abe4481af24e389dc9843144a
Mahmoud Bazzal <mahmood1994ha@gmail.com>
2019-08-23 09:14:44 +0200

    fixed copyrights

--- parallellaDeploy.sh
+++ parallellaDeploy.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+#*******************************************************************************
+#   Copyright (c) 2019 Dortmund University of Applied Sciences and Arts and others.
+#   
+#   This program and the accompanying materials are made
+#   available under the terms of the Eclipse Public License 2.0
+#   which is available at https://www.eclipse.org/legal/epl-2.0/
+#   
+#   SPDX-License-Identifier: EPL-2.0
+#   
+#   Contributors:
+#       Dortmund University of Applied Sciences and Arts - initial API and implementation
+#*******************************************************************************
 echo "Parallella Deployment Script version 1.0"
 echo "welcome to the world of tomorrow"
 
